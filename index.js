a7f80517fd7d12c2dff1d46127d0c881de5a6de2
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-13 19:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  get circumference() {
+    return this.diameter * Math.PI
+  }
+
+  get area() {
+    return Math.PI * Math.pow(this.radius, 2)
+  }
+}
